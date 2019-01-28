c5ae06cd0bc8e778a360ecf23c7ec49964ae060d
Ammar Ali <funnyguy118@gmail.com>
2019-01-28 03:05:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 def reverse_each_word (sentence)
   sentence.split.collect{|word| word.reverse}.join(" ")
-  
-  #sentence_ary = sentence.split(/ /)
-  #updated_sentence = []
+end
+
+  #sentence_ary = sentence.split
   #sentence_ary.each do |word|
   #  sentence_ary << word.reverse 
   #end 
   #sentence_ary.join(" ")
-end
